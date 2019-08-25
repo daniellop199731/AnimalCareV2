69dc7b73edc832612e901d79e463fafb79f84a7b
SaraOrozco <sara.vanesa@hotmail.com>
2019-08-24 19:31:24 -0500

    Commit search view

--- app/src/main/java/com/example/animalcarev2/BuscarAnimal.java
+++ app/src/main/java/com/example/animalcarev2/BuscarAnimal.java
@@ -3,12 +3,29 @@ package com.example.animalcarev2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class BuscarAnimal extends AppCompatActivity {
+    //Vistas o componentes que se usaran
+    EditText txtresultado, txtcodigoAnimal;
+    Button btnBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_animal);
+        iniViwes();
+    }
+    /**
+     *  Metodo que se usa para iniciar las vistas o componentes, en el metodo OnCreate
+     */
+    public void iniViwes(){
+        txtcodigoAnimal = (EditText) findViewById(R.id.txtcodigoAnimal);
+        btnBuscar = (Button) findViewById(R.id.btnBuscar);
+        txtresultado = (EditText) findViewById(R.id.txtresultado);
+
     }
 }
--- app/src/main/java/com/example/animalcarev2/MainActivity.java
+++ app/src/main/java/com/example/animalcarev2/MainActivity.java
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //intent = new Intent(getApplicationContext(), BuscarAnimal.class);
-                //startActivity(intent);
+                intent = new Intent(getApplicationContext(), BuscarAnimal.class);
+                startActivity(intent);
             }
         });
     }
